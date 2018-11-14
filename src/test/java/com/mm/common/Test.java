6cd07b964310a3815423f6b4e7d3fbb2bb1b272d
毛有斌 <maoyoubin@hoolai.com>
2018-11-14 20:56:33 +0800

    dd

--- src/test/java/com/mm/common/Test.java
+++ src/test/java/com/mm/common/Test.java
@@ -2,13 +2,10 @@ package com.mm.common;
 
 import java.io.IOException;
 
-import com.mm.scanner.PathMatchingResource;
-
 public class Test {
 
 	public static void main(String[] args) throws IOException {
-		PathMatchingResource resource = new PathMatchingResource();
-		 resource.getResources();
+		
 
 	}
 
